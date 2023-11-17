ba034a428cf3d53518727a6f9168e2f31c2efcb1
DanFilipe2000 <anielilipe90@gmail.com>
2023-11-17 20:12:48 -0300

    implementando class Noh

---
+++ src/scripts/doubleLinkedList.js
@@ -0,0 +1,18 @@
+const Noh = require("./noh.js");
+
+// O comportamento da lista será de fila
+
+class DoubleLinkedList {
+    constructor() {
+        this.cabeeca = null;
+        this.cauda = null;
+    }
+
+    insert_start() {
+        
+    }
+}
+
+module.exports = {
+    DoubleLinkedList,
+}
---
+++ src/scripts/noh.js
@@ -0,0 +1,11 @@
+class Noh {
+    constructor(valor) {
+        this.valor = valor;
+        this.proximo = null;
+        this.proximo = null;
+    }
+}
+
+module.exports = {
+    Noh,
+}
