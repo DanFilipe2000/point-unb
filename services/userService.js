718aad4b35511cff2ac7aa0a3c031c24d9a70c03
Yuri Arruda <114200839+pyurini@users.noreply.github.com>
2023-11-28 16:56:10 -0300

    tela de feed

---
+++ services/userService.js
@@ -0,0 +1,50 @@
+const { ListaDuplamenteEncadeada } = require("../scripts/doubleLinkedList.js");
+const { recuperarDados, inserirDado } = require("../auxs/processCSV.js");
+const criarIdUnico = require("../auxs/createUniqueId.js");
+
+const listaLocais = new ListaDuplamenteEncadeada();
+
+const localCSVPath = "database/tabelas/user.csv";
+
+const popularLista = () => {
+    const dados = recuperarDados(localCSVPath);
+
+    dados.forEach((element) => {
+        listaLocais.inserir_final(element);
+    });
+};
+
+const recuperarLugares = () => {
+    return listaLocais.retorna_tudo();
+};
+
+const cadastraLugar = (body, file) => {
+    const new_body = body;
+
+    if (file) {
+        const fotoPath = file.path;
+
+        new_body["foto"] = fotoPath;
+    };
+
+    new_body["id"] = criarIdUnico();
+
+    listaLocais.inserir_final(new_body);
+
+    listaLocais.travessia();
+
+    listaLocais.ordena_por_nota();
+
+    const dados_para_cadastrar = listaLocais.retorna_tudo();
+
+    inserirDado(localCSVPath, dados_para_cadastrar);
+
+    return new_body;
+};
+
+module.exports = {
+    listaLocais,
+    cadastraLugar,
+    recuperarLugares,
+    popularLista,
+};
